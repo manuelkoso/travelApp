e1dc5bbeb1ff06711fb92290cbcd78b9f4bedc49
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-07-01 17:27:26 +0200

    Add configuration to automize npm building

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,7 @@
+const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/webApp/" : "/",
+  outputDir: path.resolve(__dirname, "../backend/src/main/webapp"),
 });
