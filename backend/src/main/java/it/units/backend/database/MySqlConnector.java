031aca94c0ddf2748ae9e0b033082d170672a072
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-07-04 17:40:26 +0200

    Solved security issues: now the database username and the password are stored in the tomcat configuration file config.xml

--- backend/src/main/java/it/units/backend/database/MySqlConnector.java
+++ backend/src/main/java/it/units/backend/database/MySqlConnector.java
@@ -1,22 +1,27 @@
 package it.units.backend.database;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class MySqlConnector implements DBConnector {
 
-    final static private String connectionString = "jdbc:mysql://localhost:3306/travel_app?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&jdbcCompliantTruncation=false";
-    final static private String usernameDB = "root";
-    final static private String passwordDB = "comp55datIncredibile";
     private Connection connection;
+
+    private DataSource dataSource = null;
 
     @Override
     public Connection connect() {
+
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connection = DriverManager.getConnection(connectionString, usernameDB, passwordDB);
-        } catch (ClassNotFoundException | SQLException e) {
+            Context context = new InitialContext();
+            Context envContext = (Context) context.lookup("java:/comp/env");
+            this.dataSource = (DataSource) envContext.lookup("jdbc/travel_app");
+            connection = dataSource.getConnection();
+        } catch (Exception e) {
             System.err.println(e);
         }
         return connection;
