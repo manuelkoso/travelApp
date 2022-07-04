d05a204a0f3cdab74204e4b0303b3c337b03812a
Manuel Kosovel <kosovelmanuel@gmail.com>
2022-07-04 18:40:10 +0200

    Replace exception type

--- backend/src/main/java/it/units/backend/database/MySqlConnector.java
+++ backend/src/main/java/it/units/backend/database/MySqlConnector.java
@@ -2,24 +2,22 @@ package it.units.backend.database;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class MySqlConnector implements DBConnector<Connection> {
-
     private Connection connection;
-
     @Override
     public Connection connect() {
-
-        DataSource dataSource = null;
 
         try {
             Context context = new InitialContext();
             Context envContext = (Context) context.lookup("java:/comp/env");
-            dataSource = (DataSource) envContext.lookup("jdbc/travel_app");
+            DataSource dataSource = (DataSource) envContext.lookup("jdbc/travel_app");
             connection = dataSource.getConnection();
-        } catch (Exception e) {
+        } catch (SQLException | NamingException e) {
             System.err.println(e);
         }
         return connection;
